abe1fa6bfd3d08179e28aa3aa430543da28c81f1
Woodro420 <49668772+Woodro420@users.noreply.github.com>
2019-04-16 01:44:49 -0600

    Party handling added

--- Characters/Heawon.js
+++ Characters/Heawon.js
@@ -1,3 +1,36 @@
+//Party Handler
+var group = ["Woodro", "Heawon", "Leslie"];
+
+setInterval(function () {
+    if (character.name == group[0]) {
+        for (let i = 1; i < group.length; i++) {
+            let name = group[i];
+            send_party_invite(name);
+        }
+    } else {
+        if (character.party) {
+            if (character.party != group[0]) {
+                parent.socket.emit("party", {event: "leave"})
+            }
+        } else {
+            send_party_request(group[0]);
+        }
+    }
+}, 1000 * 10);
+
+function on_party_request(name) {
+    console.log("Party Request");
+    if (group.indexOf(name) != -1) {
+        accept_party_request(name);
+    }
+}
+function on_party_invite(name) {
+    console.log("Party Invite");
+    if (group.indexOf(name) != -1) {
+        accept_party_invite(name);
+    }
+}
+
 //Send Items to merchant if in range of character
 setInterval(function ()
 {
