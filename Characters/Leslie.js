d1c9691e901cb0131ebab9bc4f8d02c83309846f
Woodro420 <49668772+Woodro420@users.noreply.github.com>
2019-04-16 00:55:22 -0600

    Edited text

---
+++ Characters/Leslie.js
@@ -0,0 +1,114 @@
+load_code(99)
+//Send Items to merchant if in range of character
+setInterval(function ()
+{
+    let items = parent.character.items
+    let player = get_player("Woegraf");
+    if (player == null) return;
+    if (player.visible == null) return;
+    for(let i = 2; i < 40; i++) {
+        if ((items[i]) != null) {
+            send_item(player, i, 1);
+			send_gold(player, 10000000);
+        }
+    }
+}, 1000);
+
+//Party Handler
+setInterval(function() {
+	accept_party_invite("Woodro");
+	send_party_invite("Woodro");
+}, 1000*60);
+
+//Toggle for allowing the combat part of the code to execute whilst running.
+var attack_mode = true
+
+/*
+Everything within the braces of the function combatScript() is the syntax that
+will make your character do things, in this case fight.
+*/
+function combatScript(){
+	/*
+	To stop your character from using a potion everytime you lose a little bit
+	of health or mana we need to make sure it only happens when you are
+	below a minimum amount of health or mana. Note that once it executes it
+	will always cause your character to use both HP and MP regardless of
+	the amount of health or mana lost.
+	*/
+
+	//Current treshold for using a Health Potion
+	var useHPTreshold = character.max_hp / 2;
+	//Current treshold for using a Mana Potion
+	var useMPTreshold = character.max_mp / 2;
+
+	/*
+	If your mana or health is below its treshold use a HP and MP if applicable
+	*/
+	    if (character.hp / character.max_hp < 0.50 || character.mp /  character.max_mp < 0.25) {
+        use_hp_or_mp();
+    }
+	//Loot everything in your current proximity
+	loot();
+
+	/*
+	If you are moving, dead or not attacking don't execute the rest of the
+	script.
+	*/
+	if (is_moving(character) || character.rip || !attack_mode) return;
+
+	//Declare your current target
+	var target = get_targeted_monster();
+    var Woodro = get_player("Woodro")
+	//If you currently have no target.
+	if (!target) {
+		//Aquire a new target and output it to the console.
+		set_message("Targeting monster!");
+		target = get_target_of(Woodro);
+		if (is_monster(target)) {
+		   if (target) change_target(target);
+		}
+		else {
+			//If there are no targets available output it to the console.
+			set_message("Nothing to target!");
+			move(
+			character.x + ((Woodro.x - character.x) + 0),
+			character.y + ((Woodro.y - character.y) - 15)
+			);
+			return;
+		}
+	}
+
+
+	//If not in attack range of current target.
+	if (!in_attack_range(target)) {
+
+		//Move half the distance towards the target.
+		move(
+			character.x + ((target.x - character.x) / 2),
+			character.y + ((target.y - character.y) / 2)
+			);
+	}
+	//If in attack range, attack and output to the console.
+	else if (can_attack(target)) {
+
+		set_message("Attacking");
+		attack(target);
+	}
+}
+
+/*
+To prevent the communication between the server and your gameclient from
+choking we need to limit how often our code runs, so the server has time to
+process things. Below we will determine the amount of milliseconds that need to
+pass before the script is allowed to execute again (this is called a cycletime).
+*/
+
+// Loops every 1/4 seconds a.k.a. a cycletime of 250ms
+var cycleTime = (1000/4);
+
+/*
+Everything within the braces of setInterval() is executed at the chosen
+interval (through the "cycletime" parameter). This is where we call the
+combatScript function so it starts running.
+*/
+setInterval(combatScript, cycleTime);
---
+++ Characters/Woodro.js
@@ -0,0 +1,275 @@
+load_code(99)
+//Target ID
+var monster_targets = ["mvampire",
+					   	  "bat",
+					      "goldenbat"
+					     ];
+
+//Send Items to merchant if in range of character
+setInterval(function ()
+{
+    let items = parent.character.items
+    let player = get_player("Woegraf");
+    if (player == null) return;
+    if (player.visible == null) return;
+    for(let i = 2; i < 40; i++) {
+        if ((items[i]) != null) {
+            send_item(player, i, 1);
+			send_gold(player, 10000000)
+        }
+    }
+}, 1000);
+
+//Party Handler
+setInterval(function() {
+	send_party_invite("Heawon");
+	send_party_invite("Leslie");
+	accept_party_invite("Leslie");
+	accept_party_invite("Heawon");
+					  }, 1000*60);
+
+var state = "farm";
+var min_potions = 50; //The number of potions at which to do a resupply run.
+var purchase_amount = 50;//How many potions to buy at once.
+var potion_types = ["hpot0", "mpot0"];//The types of potions to keep supplied.
+
+//Movement And Attacking
+setInterval(function () {
+
+	//Determine what state we should be in.
+	state_controller();
+
+	//Switch statement decides what we should do based on the value of 'state'
+	switch(state)
+	{
+		case "farm":
+			farm();
+			break;
+		case "resupply_potions":
+			resupply_potions();
+			break;
+	}
+}, 100);//Execute 10 times per second
+
+//Potions And Looting
+setInterval(function () {
+    loot();
+
+    //Heal With Potions if we're below 75% hp.
+    if (character.hp / character.max_hp < 0.50 || character.mp / character.max_mp < 0.25) {
+        use_hp_or_mp();
+    }
+}, 500 );//Execute 2 times per second
+
+function state_controller()
+{
+	//Default to farming
+	var new_state = "farm";
+
+	//Do we need potions?
+	for(type_id in potion_types)
+	{
+		var type = potion_types[type_id];
+
+		var num_potions = num_items(type);
+
+		if(num_potions < min_potions)
+		{
+			new_state = "resupply_potions";
+			break;
+		}
+	}
+
+	if(state != new_state)
+	{
+		state = new_state;
+	}
+
+}
+
+//This function contains our logic for when we're farming mobs
+function farm()
+{
+	var target = find_viable_targets()[0];
+	//Attack or move to target
+    if (target != null) {
+        if (distance_to_point(target.real_x, target.real_y) < character.range) {
+            if (can_attack(target)) {
+                attack(target);
+            }
+        }
+        else {
+            move_to_target(target);
+        }
+	}
+	else
+	{
+		if (!smart.moving) {
+			game_log("finding a target");
+            smart_move({ to: monster_targets[0] });
+        }
+	}
+}
+
+//This function contains our logic during resupply runs
+function resupply_potions()
+{
+	var potion_merchant = get_npc("fancypots");
+
+	var distance_to_merchant = null;
+
+	if(potion_merchant != null)
+	{
+		distance_to_merchant = distance_to_point(potion_merchant.position[0], potion_merchant.position[1]);
+	}
+
+	if (!smart.moving
+		&& (distance_to_merchant == null || distance_to_merchant > 250)) {
+            smart_move({ to:"potions"});
+    }
+
+	if(distance_to_merchant != null
+	   && distance_to_merchant < 250)
+	{
+		buy_potions();
+	}
+}
+
+//Buys potions until the amount of each potion_type we defined in the start of the script is above the min_potions value.
+function buy_potions()
+{
+	if(empty_slots() > 0)
+	{
+		for(type_id in potion_types)
+		{
+			var type = potion_types[type_id];
+
+			var item_def = parent.G.items[type];
+
+			if(item_def != null)
+			{
+				var cost = item_def.g * purchase_amount;
+
+				if(character.gold >= cost)
+				{
+					var num_potions = num_items(type);
+
+					if(num_potions < min_potions)
+					{
+						buy(type, purchase_amount);
+					}
+				}
+				else
+				{
+					game_log("Not Enough Gold!");
+				}
+			}
+		}
+	}
+	else
+	{
+		game_log("Inventory Full!");
+	}
+}
+
+
+//Returns the number of items in your inventory for a given item name;
+function num_items(name)
+{
+	var item_count = character.items.filter(item => item != null && item.name == name).reduce(function(a,b){ return a + (b["q"] || 1);
+	}, 0);
+
+	return item_count;
+}
+
+//Returns how many inventory slots have not yet been filled.
+function empty_slots()
+{
+	return character.esize;
+}
+
+//Gets an NPC by name from the current map.
+function get_npc(name)
+{
+	var npc = parent.G.maps[character.map].npcs.filter(npc => npc.id == name);
+
+	if(npc.length > 0)
+	{
+		return npc[0];
+	}
+
+	return null;
+}
+
+//Returns the distance of the character to a point in the world.
+function distance_to_point(x, y) {
+    return Math.sqrt(Math.pow(character.real_x - x, 2) + Math.pow(character.real_y - y, 2));
+}
+
+//This function will ether move straight towards the target entity,
+//or utilize smart_move to find their way there.
+function move_to_target(target) {
+    if (can_move_to(target.real_x, target.real_y)) {
+        smart.moving = false;
+        smart.searching = false;
+        move(
+            character.real_x + (target.real_x - character.real_x) / 2,
+            character.real_y + (target.real_y - character.real_y) / 2
+        );
+    }
+    else {
+        if (!smart.moving) {
+            smart_move({ x: target.real_x, y: target.real_y });
+        }
+    }
+}
+
+//Returns an ordered array of all relevant targets as determined by the following:
+////1. The monsters' type is contained in the 'monsterTargets' array.
+////2. The monster is attacking you or a party member.
+////3. The monster is not targeting someone outside your party.
+//The order of the list is as follows:
+////Monsters attacking you or party members are ordered first.
+////Monsters are then ordered by distance.
+function find_viable_targets() {
+    var monsters = Object.values(parent.entities).filter(
+        mob => (mob.target == null
+                    || parent.party_list.includes(mob.target)
+                    || mob.target == character.name)
+                && (mob.type == "monster"
+                    && (parent.party_list.includes(mob.target)
+                        || mob.target == character.name))
+                    || monster_targets.includes(mob.mtype));
+
+    for (id in monsters) {
+        var monster = monsters[id];
+
+        if (parent.party_list.includes(monster.target)
+                    || monster.target == character.name) {
+            monster.targeting_party = 1;
+        }
+        else {
+            monster.targeting_party = 0;
+        }
+    }
+
+    //Order monsters by whether they're attacking us, then by distance.
+    monsters.sort(function (current, next) {
+        if (current.targeting_party > next.targeting_party) {
+            return -1;
+        }
+        var dist_current = distance(character, current);
+        var dist_next = distance(character, next);
+        // Else go to the 2nd item
+        if (dist_current < dist_next) {
+            return -1;
+        }
+        else if (dist_current > dist_next) {
+            return 1
+        }
+        else {
+            return 0;
+        }
+    });
+    return monsters;
+}
