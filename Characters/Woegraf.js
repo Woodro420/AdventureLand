9715dbd8b442f3cf1c398e2cdbec0bed609a0c0f
Woodro420 <49668772+Woodro420@users.noreply.github.com>
2019-04-17 23:51:25 -0600

    position changes

--- Characters/Woegraf.js
+++ Characters/Woegraf.js
@@ -1,6 +1,6 @@
 //change these to determine your sitting spot in town you want these to match whats in your cmstuff.js
-var standx = -250
-var standy = -100.0000001
+var standx = -207
+var standy = -68
 
 setInterval(function () {
   items = character.items
