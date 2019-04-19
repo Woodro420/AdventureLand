ca3cd09289c5d1376245c1c12d23ef9d1b5d6938
Woodro420 <49668772+Woodro420@users.noreply.github.com>
2019-04-18 22:14:46 -0600

    oneeye added

--- Characters/Woodro.js
+++ Characters/Woodro.js
@@ -50,7 +50,7 @@ function on_party_invite(name) {
 
 //Target ID
 var priority_targets = ["phoenix", "mvampire", "goldenbat"]
-var monster_targets = ["bat", "cgoo"];
+var monster_targets = ["bat", "cgoo", "oneye"];
 
 //calls merchant
 setInterval(function () {
